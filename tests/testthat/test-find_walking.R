d7fade2d751b319c381e559930593f80d5033d3d
muschellij2 <muschellij2@gmail.com>
2023-08-11 13:33:19 -0400

    added test for use case

--- tests/testthat/test-find_walking.R
+++ tests/testthat/test-find_walking.R
@@ -12,4 +12,24 @@ testthat::test_that("find_walking gives fixed answer", {
     round(out$steps, 10),
     c(1.65, 1.6, 1.55, 1.6, 1.55, 1.85, 1.8, 1.75, 1.75, 1.7)
   )
+
+  xdata = data
+
+  data = xdata
+
+  data = data %>%
+    # need use case for standardize data
+    dplyr::rename(HEADER_TIMESTAMP = time) %>%
+    # need use case in mm/s^2
+    dplyr::mutate(
+      x = x * 9.80665,
+      y = y * 9.80665,
+      z = z * 9.80665
+      )
+  suppressWarnings({out2 = find_walking(data, sample_rate = 10L)})
+  testthat::expect_identical(
+    round(out$steps, 10),
+    c(1.65, 1.6, 1.55, 1.6, 1.55, 1.85, 1.8, 1.75, 1.75, 1.7)
+  )
+
 })
