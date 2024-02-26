0147bd9267a49cf9d2da96587d08e151ed63bd01
muschellij2 <muschellij2@gmail.com>
2024-02-26 12:38:06 -0500

    added forest_version_pip

--- R/install_forest.R
+++ R/install_forest.R
@@ -11,6 +11,7 @@
 #' @examples
 #' if (have_forest()) {
 #'    forest_version()
+#'    forest_version_pip()
 #' }
 #'
 install_forest = function(ref = "develop",
@@ -32,18 +33,48 @@ have_forest = function() {
 }
 
 
-module_version = function(module = "numpy") {
+module_info = function(module = "numpy") {
   assertthat::is.scalar(module)
   if (!reticulate::py_module_available(module)) {
     stop(paste0(module, " is not installed!"))
   }
   df = reticulate::py_list_packages()
-  df$version[df$package == module]
+  df[df$package == module,]
+}
+
+module_version = function(module = "numpy") {
+  module_info(module = module)$version
+}
+
+module_requirement = function(module = "numpy") {
+  module_info(module = module)$requirement
 }
 
 #' @export
 #' @rdname forest_setup
 forest_version = function() {
   module_version("forest")
+}
+
+module_version_pip = function(module = "numpy") {
+  python = reticulate::py_config()$python
+  python = shQuote(python)
+  cmd = paste0(python, " -m pip freeze")
+  out = system(cmd, intern = TRUE)
+  out = out[grep(paste0("^", module, "(=|\\s|@)"), out)]
+  out
+}
+
+#' @export
+#' @rdname forest_setup
+forest_version_pip = function() {
+  module_version_pip("forest")
+
+}
+
+#' @export
+#' @rdname forest_setup
+forest_requirement = function() {
+  module_requirement("forest")
 }
 
