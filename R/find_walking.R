7e318fd224c4c6baa9a6c7183bd312792f8719b5
muschellij2 <muschellij2@gmail.com>
2026-07-09 10:35:23 -0400

    added in donttest for find_walking

--- R/find_walking.R
+++ R/find_walking.R
@@ -26,11 +26,13 @@
 #' @export
 #'
 #' @examples
-#' csv_file = system.file("test_data_bout.csv", package = "walking")
-#' if (requireNamespace("readr", quietly = TRUE) && reticulate::py_module_available("forest")) {
-#'   x = readr::read_csv(csv_file)
-#'   colnames(x)[colnames(x) == "UTC time"] = "time"
-#'   res = find_walking(data = x)
+#' \donttest{
+#'   csv_file = system.file("test_data_bout.csv", package = "walking")
+#'   if (requireNamespace("readr", quietly = TRUE) && reticulate::py_module_available("forest")) {
+#'     x = readr::read_csv(csv_file)
+#'     colnames(x)[colnames(x) == "UTC time"] = "time"
+#'     res = find_walking(data = x)
+#'   }
 #' }
 find_walking = function(
     data,
