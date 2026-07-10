30e91f0727f6dbae293b38540e58617d168dad15
muschellij2 <muschellij2@gmail.com>
2026-07-10 14:07:46 -0400

    trying like cran

--- tests/testthat/setup-forest.R
+++ tests/testthat/setup-forest.R
@@ -1,1 +1,10 @@
+# Call py_require_forest but do cleanup for CRAN
+files = list.files()
 walking::py_require_forest()
+reticulate::import("forest")
+new_files = list.files()
+sd = setdiff(new_files, files)
+sd = sd[grepl("^uv.*[.]lock", sd)]
+if (length(sd) > 0) {
+  file.remove(sd)
+}
